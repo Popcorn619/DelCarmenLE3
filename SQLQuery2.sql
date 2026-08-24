c44b30be4609409627d55278f86ecfc96e63c884
Popcorn619 <joshdelcarmen2006@gmail.com>
2026-08-24 10:10:31 +0800

    Final Lab Submission

---
+++ SQLQuery2.sql
@@ -0,0 +1,4 @@
+﻿INSERT INTO dbo.Posts (UserId, Title, Body, DateCreated)
+VALUES 
+(3, 'My Second Post', 'This is my second blog post. Hello everyone!', GETDATE()),
+(4, 'Tips for Beginners', 'Here are some useful tips for new developers...', GETDATE());
